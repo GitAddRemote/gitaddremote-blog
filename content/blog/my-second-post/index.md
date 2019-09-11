fb11bbdd80f433e135949258eca9229dc63164f4
CommanderQ <commanderq@pop-os.localdomain>
2019-09-10 22:09:53 -0400

    Finish updating with my information instead of the default from the template.

--- content/blog/my-second-post/index.md
+++ content/blog/my-second-post/index.md
@@ -1,7 +1,7 @@
 ---
 title: My Second Post!
 date: "2015-05-06T23:46:37.121Z"
-published: true
+published: false
 ---
 
 Wow! I love blogging so much already.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `GitAddRemote`,
+    author: `Demian`,
+    description: `A blog dedicated to software development and management practices from a remote work perspective.`,
+    siteUrl: `https://gitaddremote.github.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `gitaddremote`,
     },
   },
   plugins: [
